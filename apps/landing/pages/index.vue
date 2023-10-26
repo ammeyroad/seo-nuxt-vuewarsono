88623341a86c533fccb41959a442a39101dd8296
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-10-26 19:54:23 +0700

    Initial commit

---
+++ apps/landing/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'landing',
+})
+
+useHead({
+  title: 'Home',
+})
+</script>
+
+<template>
+  <div>
+  <p>sadsadsa</p>
+    <LandingFeatureTwoColumn />
+    <p>sadsadsa</p>
+    <LandingFeatureGrid />
+
+    <LandingCta />
+  </div>
+</template>
